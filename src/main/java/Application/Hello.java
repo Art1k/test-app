ee41517a8b95fd628a184a303adfa44770d4f230
Art1k <danny.kanevsky@gmail.com>
2017-08-13 10:28:01 +0000

    Initial commit

---
+++ src/main/java/Application/Hello.java
@@ -0,0 +1,12 @@
+/*
+ * Hello World
+ * @author danielk
+ */
+
+package Application;
+
+public class Hello {
+  public static void main( String[] args ){
+    System.out.println( "Hello World!" );
+  }
+}
